d49acb25f9f461a2fdfe4ac55f6719026b5f3ad6
devpedroval <prmval@gmail.com>
2024-01-12 21:41:34 -0300

    Add video card component with initial data

--- src/app/Components/VideoCards.tsx
+++ src/app/Components/VideoCards.tsx
@@ -1,4 +1,17 @@
+const allVideos = [ {
+    type: 'video',
+    title: 'Vídeo teste',
+    text: 'text',
+    link: 'https://www.youtube.com/watch?v=nU2p04TQJNI',
+    thumbnail: 'https://i.ytimg.com/vi/nU2p04TQJNI/maxresdefault.jpg',
+    categories: 'video',
+}];
+
+
 export default function VideoCards({category, search}: {category: string, search: string}) {
+  
+
+  
   return (
     <div>
       Vídeos
