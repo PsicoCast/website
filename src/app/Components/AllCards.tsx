55c85e7637cad9a4adc7c5e8e8c4dace577d74f8
devpedroval <prmval@gmail.com>
2024-01-15 15:36:42 -0300

    Add image optimization and fix image rendering issue

--- src/app/Components/AllCards.tsx
+++ src/app/Components/AllCards.tsx
@@ -1,4 +1,5 @@
 import { mockArray } from '../../Mocks/posts';
+import Image from 'next/image';
 
 export default function AllCards({category, search}: {category: string, search: string}) {
 
@@ -12,7 +13,10 @@ export default function AllCards({category, search}: {category: string, search: 
     <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
       {filteredPosts.sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime()).map((post, index) => (
         <div key={index} className="border p-4 rounded-lg">
-          <img src={post.thumb} alt={post.title} className="w-full h-32 object-contain mb-2"/>
+          <div className="w-full h-32 relative mb-2">
+            <Image src={post.thumb} alt={post.title} layout="fill" objectFit="contain"/>
+          </div>
+          {/* <img src={post.thumb} alt={post.title} className="w-full h-32 object-contain mb-2"/> */}
           <h1>{post.type}</h1>
           <h2 className="text-xl font-bold">{post.title}</h2>
           <p>Postado em {post.createdAt.toLocaleDateString()}</p>
