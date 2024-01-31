c7883f61c4e280fa8c43fdc4c93cc5a62f094372
devpedroval <prmval@gmail.com>
2024-01-31 14:20:25 -0300

    remove unused styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,6 @@ import ModulesList from "./Components/ModulesList";
 import Sidebar from "./Components/Sidebar/Sidebar";
 import Image from 'next/image';
 
-const sectionButtons = 'text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700';
-
 export default function Page() {
   const [ search, setSearch ] = useState('');
   const [ type, setType ] = useState('todos');
